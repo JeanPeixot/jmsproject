2711d020015ddc28d8867c69913bf598f716b1e0
Jean Peixoto <jeanpeixoto70@gmail.com>
2024-12-28 17:55:10 -0300

    adding functions to the modal

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function acionouOBotao(){
-    
+const modal = document.querySelector(".modal")
+const mascara = document.querySelector(".mascara-modal")
+
+
+function mostrarModal(){
+    modal.style.left = '50%'
+    mascara.style.visibility = 'visible'
+}
+
+function esconderModal(){
+    modal.style.left = '-30%'
+    mascara.style.visibility = 'hidden'
+
 }
